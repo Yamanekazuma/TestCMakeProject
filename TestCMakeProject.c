86ea60ce96fa5a1a7b3fa001c3170fdb3c2bce72
Yamanekazuma <cmj14141@ict.nitech.ac.jp>
2023-08-21 18:10:52 +0900

    clang-format追加

--- TestCMakeProject.c
+++ TestCMakeProject.c
@@ -1,4 +1,9 @@
-﻿#include "TestCMakeProject.h"
+﻿#include <stdlib.h>
+#include "TestCMakeProject.h"
+#include <stdio.h>
+#include <assert.h>
+#include <Windows.h>
+
 
 int main(void) {
 	printf("Hello, World!");
